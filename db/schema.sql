98e1ff19f4bcc2cbff36c59b942126eb0fc8aca0
ParksFG <ParkerFGay@gmail.com>
2022-09-15 10:40:42 -0500

    schema.sql + seeds.sql finished

--- db/schema.sql
+++ db/schema.sql
@@ -8,11 +8,11 @@ CREATE TABLE department (
     depName VARCHAR(30)
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
