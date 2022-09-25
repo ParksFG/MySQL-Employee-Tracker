a0f91c02d1d634e07d46383f10dc842d112426d8
ParksFG <ParkerFGay@gmail.com>
2022-09-24 21:02:36 -0500

    First question function added

--- index.js
+++ index.js
@@ -40,4 +40,58 @@ function appQuestions() {
                 actions.quit
             ]
         })
+        .then(answer => {
+            switch (answer.selected) {
+                case actions.viewAllEmployees:
+                    viewAllEmployees();
+                    break;
+
+                case actions.viewByDepartment:
+                    viewByDepartment();
+                    break;
+
+                case actions.viewByManager:
+                    viewByManager();
+                    break;
+
+                case actions.addEmployee:
+                    addEmployee();
+                    break;
+                    
+                case actions.addRole:
+                    addRole();
+                    break;
+                    
+                case actions.addDepartment:
+                    addDepartment();
+                    break;
+                    
+                case actions.updateEmployeeRole:
+                    updateEmployeeRole();
+                    break;
+                    
+                case actions.quit:
+                    console.log('Goodbye!')
+                    process.exit();
+                    break;
+            }
+        });
+}
+
+appQuestions()
+
+function viewAllEmployees() {
+    const query = `SELECT employee.id as ID, CONCAT(employee.last_name, ', ', employee.first_name) AS 'Employee Name', role.title as Position, department.depName AS Department, role.salary AS Salary, IFNULL(CONCAT(manager.last_name, ', ', manager.first_name), 'N/A') AS Manager
+    FROM employee
+    LEFT JOIN employee manager ON manager.id = employee.manager_id
+    JOIN role 
+    ON employee.role_id = role.id
+    JOIN department
+    ON role.department_id = department.id;`;
+    db.query(query, (err, res) => {
+        console.log(`\n`);
+        console.table(res);
+        appQuestions();
+    })
+
 }
